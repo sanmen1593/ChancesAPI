c41fa6dabc89a07163e17610df1ff41c27520fe2
Santiago Mendoza Ramirez <santiagomendozar@gmail.com>
2014-09-26 22:38:22 -0500

    Desactivar filtro csrf
    Desactivar el filtro csrf para las sesiones.

--- app/filters.php
+++ app/filters.php
@@ -79,9 +79,10 @@ Route::filter('guest', function() {
   | session does not match the one given in this request, we'll bail.
   |
  */
-
+/*
 Route::filter('csrf', function() {
     if (Session::token() != Input::get('_token')) {
         throw new Illuminate\Session\TokenMismatchException;
     }
 });
+*/
